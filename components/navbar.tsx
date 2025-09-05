437b3804e5580a53f143596445d3e2e6eb853bce
jim <wksunshine@gmail.com>
2025-09-06 00:20:45 +1000

    Create dashboard page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,32 @@
 "use client"
 
-import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs"
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs"
 import { ArrowRight, Trello } from "lucide-react"
 import { Button } from "./ui/button"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const {isSignedIn, user} = useUser(); 
+  const pathname = usePathname()
+
+  const isDashboardPage = pathname === "/dashboard"
+  const isBoardPage = pathname.startsWith("/boards/")
+
+  if (isDashboardPage){
+    return <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+    <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+      <div className="flex items-center space-x-2">
+        <Trello className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
+        <span className="text-xl sm:text-2xl font-bold text-gray-900">Dashboard</span>
+      </div>
+
+      <div className="flex items-center space-x-2 sm:space-x-4">
+        <UserButton />
+      </div>
+    </div>
+  </header>
+  }
 
   return <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
     <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
